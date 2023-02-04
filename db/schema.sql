352217dc2ef8c5415fc2b2839b6deda87e6f75bd
Lon Heligas <lon.heligas@gmail.com>
2023-02-03 20:27:01 -0600

    finished the database schema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,30 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
-  name VARCHAR(30)
+  id INT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  deaprtment_id: INT,
-  FOREIGN KEY (deaprtment_id)
+  department_id INT,
+  FOREIGN KEY (department_id)
   REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id)
   REFERENCES role(id),
-  FOREIGN KEY manager_id
-  REFERENCES employee(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id),
+  PRIMARY KEY (id)
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -23,24 +23,28 @@ VALUES (1, "Field Officer", "1000000", 1),
        (15, "Infrastructure Ops", "60000", 5);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Nicholas", "Fury", 1),
-       (1, "Phil", "Coulson", 1, 1),
-       (1, "Jemma", "Simmons", 3, 2),
-       (1, "Leopold", "Fitzgerald", 4, 2),
-       (1, "Melinda", "May", 3, 2),
-       (1, "Grant", "Ward", 3, 2),
-       (1, "Daisy", "Johnson", 3, 2),
-       (1, "Alfonzo", "Mackenzie", 5, 2),
-       (1, "Elena", "Rodriguez", 3, 2),
-       (1, "Bobbie", "Morse", 2, 1),
-       (1, "Maria", "Hill", 12, 2),
-       (1, "Billy", "Koening", 11, 1),
-       (1, "Antoine", "Triplett", 3, 2),
-       (1, "Deke", "Shaw", 9, 4),
-       (1, "Victoria", "Hand", 1, 1),
-       (1, "Jack", "Warner", 13, 4),
-       (1, "Piper", "Hamilton", 10, 15),
-       (1, "David", "Campbell", 11, 15),
-       (1, "David", "Campbell", 11, 15),
+VALUES (1, "Nicholas", "Fury", 1, 1),
+       (2, "Phil", "Coulson", 1, 1),
+       (3, "Jemma", "Simmons", 3, 2),
+       (4, "Leopold", "Fitzgerald", 4, 2),
+       (5, "Melinda", "May", 3, 2),
+       (6, "Grant", "Ward", 3, 2),
+       (7, "Daisy", "Johnson", 3, 2),
+       (8, "Alfonzo", "Mackenzie", 5, 2),
+       (9, "Elena", "Rodriguez", 3, 2),
+       (10, "Bobbie", "Morse", 2, 1),
+       (11, "Maria", "Hill", 12, 2),
+       (12, "Billy", "Koening", 11, 1),
+       (13, "Antoine", "Triplett", 3, 2),
+       (14, "Deke", "Shaw", 9, 4),
+       (15, "Victoria", "Hand", 1, 1),
+       (16, "Jack", "Warner", 13, 4),
+       (17, "Piper", "Hamilton", 10, 15),
+       (18, "David", "Campbell", 11, 15),
+       (19, "Alex", "Warren", 15, 15),
+       (20, "Daniel", "Sousa", 3, 7);
+
+
+
 
        
