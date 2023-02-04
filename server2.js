1de3a35e3be8f5cc2d4ec692bbe7e79ba738a939
Lon Heligas <lon.heligas@gmail.com>
2023-02-03 21:00:10 -0600

    connected to the DB and was pulled data

--- server2.js
+++ server2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
-// const inquirer = require('inquirer');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,30 +9,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// connects to database
+// Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL: 'username';
-    user: 'root',    
-    // MySQL: password
-    password: 'bluesbrothers',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
     database: 'employees_db'
   },
   console.log(`Connected to the employees_db database.`)
 );
-  
+
 // Query database
 db.query('SELECT * FROM employee', function (err, results) {
   console.log(results);
 });
-  
-// Default response for any other request (not found)
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
-  
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} 🚀`);
-});
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
